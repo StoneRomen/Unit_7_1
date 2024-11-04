002e9d44d2e24a1c47b2658843ff7d937c150003
StoneRomen <184683610+StoneRomen@users.noreply.github.com>
2024-11-04 14:11:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Написати функцію, яка приймає 1 параметр. Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// console.log(sum(4)); // 4
+
+
+function countSum() {
+  let result = 0;
+  return function (num) {
+    result += num;
+    return result;
+  };
+}
+
+const sum = countSum();
+
+console.log(sum(4)); // 4
+console.log(sum(9)); // 13
+
